8711a29814e7b409b786152f38b09647c2cd554a
Neil Jay Warner <Neil.J.Warner@aa.com>
2016-05-28 12:30:25 -0500

    C3 - Adust share fab a bit

--- app/src/main/java/org/disciplestoday/disciplestoday/ArticleDetailActivity.java
+++ app/src/main/java/org/disciplestoday/disciplestoday/ArticleDetailActivity.java
@@ -26,7 +26,7 @@ public class ArticleDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabShareArticle);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
